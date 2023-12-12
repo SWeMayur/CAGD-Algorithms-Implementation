fb79d28677afdd730f12d24d863c64d17132f910
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2023-12-13 00:12:05 +0530

    Update OpenGLWindow.h

--- Visualizer/OpenGLWindow.h
+++ Visualizer/OpenGLWindow.h
@@ -23,25 +23,16 @@ protected:
 private:
    // void createGeometry();
     void reset();
-    void drawGrid(QVector<GLfloat>& vertices, QVector<GLfloat>& colors, float gridSize);
     void fillSquare(const QVector<QVector2D>& squareVertices, const QVector3D& fillColor);
-    void bresenhamLinePixels(float x1, float y1, float x2, float y2, QVector<QVector2D>& pixelCoordinates);
-    void SymmetricDDA(float x1, float y1, float x2, float y2, QVector<QVector2D>& pixelVertices);
 private:
-    bool mAnimating = false;
-    QOpenGLContext* mContext = nullptr;
-    QOpenGLPaintDevice* mDevice = nullptr;
 
     QOpenGLShader* mVshader = nullptr;
     QOpenGLShader* mFshader = nullptr;
     QOpenGLShaderProgram* mProgram = nullptr;
 
-    QList<QVector3D> mVertices;
-    QList<QVector3D> mNormals;
+
     QOpenGLBuffer mVbo;
-    int mVertexAttr;
-    int mNormalAttr;
-    int mMatrixUniform;
+    
     QColor mBackground;
     QMetaObject::Connection mContextWatchConnection;
 
@@ -49,7 +40,6 @@ private:
     GLint m_colAttr = 0;
     GLint m_matrixUniform = 0;
     float mFloatInputs[4];
-    Line *mLine;
     int gridSize = 0;
    // QInputDialog* mInputBox;
 public slots:
