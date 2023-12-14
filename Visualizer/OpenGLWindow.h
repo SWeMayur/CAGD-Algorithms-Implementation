f664d04516e682dcd6e8f13e826e5af512c55a87
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2023-12-14 18:09:33 +0530

    Update OpenGLWindow.h

--- Visualizer/OpenGLWindow.h
+++ Visualizer/OpenGLWindow.h
@@ -4,15 +4,17 @@
 #include <QOpenGLBuffer>
 #include "Line.h"
 
+
 class QOpenGLTexture;
 class QOpenGLShader;
 class QOpenGLShaderProgram;
-
 class QOpenGLPaintDevice;
 
 class OpenGLWindow :public QOpenGLWidget, protected QOpenGLFunctions
 {
     Q_OBJECT
+signals:
+    void shapeUpdate();
 public:
     OpenGLWindow(const QColor& background, QMainWindow* parent);
     ~OpenGLWindow();
@@ -20,29 +22,34 @@ public:
 protected:
     void paintGL() override;
     void initializeGL() override;
-private:
-   // void createGeometry();
-    void reset();
-    void fillSquare(const QVector<QVector2D>& squareVertices, const QVector3D& fillColor);
-private:
 
+private:
+    void reset();
+
+
+private:
     QOpenGLShader* mVshader = nullptr;
     QOpenGLShader* mFshader = nullptr;
     QOpenGLShaderProgram* mProgram = nullptr;
-
-
     QOpenGLBuffer mVbo;
-    
+    int mVertexAttr;
+    int mNormalAttr;
+    int mMatrixUniform;
     QColor mBackground;
     QMetaObject::Connection mContextWatchConnection;
-
     GLint m_posAttr = 0;
     GLint m_colAttr = 0;
     GLint m_matrixUniform = 0;
-    float mFloatInputs[4];
-    int gridSize = 0;
-   // QInputDialog* mInputBox;
-public slots:
-    void getUserInput();
-    void getGridInput();
+    void fillSquare(const QVector<QVector2D>& squareVertices, const QVector3D& fillColor);
+    void colorSqr(QVector<QVector2D>& pixels);
+    QVector<QVector2D> mPixelVertices;
+
+public:
+    QVector<GLfloat> vertices;
+    QVector<GLfloat> colors;
+    int gridSize;
+    void addLine(Line l);
+    void addGrid(int size);
+    void bresenhams(Line l);
+    void symmetricDDA(Line l);
 };
