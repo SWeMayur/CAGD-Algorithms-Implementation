1a4b611b18c806767438f70b9c20a40a451978d1
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2023-12-14 18:10:14 +0530

    Update Visualizer.h

--- Visualizer/Visualizer.h
+++ Visualizer/Visualizer.h
@@ -1,26 +1,61 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
-#include "Visualizer.h"
+#include "Visualize.h"
+#include "OpenGLWindow.h"
+//#include "SymmetricDDA.h"
 
 class OpenGLWindow;
 
-class Visualizer : public QMainWindow
+class Visualize : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    Visualizer(QWindow* parent = nullptr);
-    ~Visualizer();
+    Visualize(QWindow* parent = nullptr);
+    ~Visualize();
 
 private:
     void setupUi();
 
+public:
+    QVector<QVector2D> mPixelVertices;
+    QVector<GLfloat>   mVertices;
+    QVector<GLfloat>   mColors;
+
 private:
-    QMenuBar* mMenuBar;
-    QToolBar* mMainToolBar;
     QWidget* mCentralWidget;
-    QStatusBar* mStatusBar;
     OpenGLWindow* mRenderer;
 
+    QPushButton* mPushButton1;
+    QPushButton* mPushButton2;
+    QPushButton* mPushButton3;
+    QPushButton* mPushButton4;
+   
+    QLineEdit* mLineEdit1;
+    QLineEdit* mLineEdit2;
+    QLineEdit* mLineEdit3;
+    QLineEdit* mLineEdit4;
+    QLineEdit* mLineEdit5;
+   
+    QLabel* mLabel1;
+    QLabel* mLabel2;
+    QLabel* mLabel3;
+    QLabel* mLabel4;
+    QLabel* mLabel5;
+
+    float xCoordinate;
+    float yCoordinate;
+    float x1Coordinate;
+    float y1Coordinate;
+    float xMin;
+    float yMin;
+    float xMax;
+    float yMax;
+
+public slots:
+    void addLine();
+    void addGrid();
+    void bresenhamsLine();
+    void symmetricDDALine();
 };
