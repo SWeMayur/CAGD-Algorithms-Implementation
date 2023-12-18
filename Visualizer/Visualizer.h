2ece83737a079be94fe0d88a5d552f89d26775cc
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2023-12-18 23:32:03 +0530

    Update Visualizer.h for bazier and hermite curve features

--- Visualizer/Visualizer.h
+++ Visualizer/Visualizer.h
@@ -24,38 +24,50 @@ public:
     QVector<GLfloat>   mColors;
 
 private:
-    QWidget* mCentralWidget;
     OpenGLWindow* mRenderer;
 
     QPushButton* mPushButton1;
     QPushButton* mPushButton2;
     QPushButton* mPushButton3;
     QPushButton* mPushButton4;
+    QPushButton* mPushButton5;
+    QPushButton* mPushButton6;
    
     QLineEdit* mLineEdit1;
     QLineEdit* mLineEdit2;
     QLineEdit* mLineEdit3;
     QLineEdit* mLineEdit4;
     QLineEdit* mLineEdit5;
+    QLineEdit* mLineEdit6;
+    QLineEdit* mLineEdit7;
+    QLineEdit* mLineEdit8;
+    QLineEdit* mLineEdit9;
+    QLineEdit* mLineEdit10;
+    QLineEdit* mLineEdit11;
+    QLineEdit* mLineEdit12;
+    QLineEdit* mLineEdit13;
    
     QLabel* mLabel1;
     QLabel* mLabel2;
     QLabel* mLabel3;
     QLabel* mLabel4;
     QLabel* mLabel5;
+    QLabel* mLabel6;
+    QLabel* mLabel7;
+    QLabel* mLabel8;
+    QLabel* mLabel9;
+    QLabel* mLabel10;
+    QLabel* mLabel11;
+    QLabel* mLabel12;
+    QLabel* mLabel13;
 
-    float xCoordinate;
-    float yCoordinate;
-    float x1Coordinate;
-    float y1Coordinate;
-    float xMin;
-    float yMin;
-    float xMax;
-    float yMax;
+
 
 public slots:
     void addLine();
     void addGrid();
     void bresenhamsLine();
     void symmetricDDALine();
+    void bazier();
+    void hermite();
 };
