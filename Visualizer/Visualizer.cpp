e5e486fbb87a9445463f2286ba42831800b02f4e
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2023-12-18 23:31:26 +0530

    Update Visualizer.cpp for bazier and hermite curve features

--- Visualizer/Visualizer.cpp
+++ Visualizer/Visualizer.cpp
@@ -3,9 +3,8 @@
 #include "OpenGLWindow.h"
 #include "Line.h"
 #include "Grid.h"
+#include "vector"
 
-//#include "SymmetricDDA.h"
-//#include "SutherlandandCohenAlgorithm.h"
 
 Visualize::Visualize(QWindow* parent) : QMainWindow(nullptr)
 {
@@ -14,6 +13,8 @@ Visualize::Visualize(QWindow* parent) : QMainWindow(nullptr)
     connect(mPushButton2, &QPushButton::clicked, this, &Visualize::addLine);
     connect(mPushButton3, &QPushButton::clicked, this, &Visualize::bresenhamsLine);
     connect(mPushButton4, &QPushButton::clicked, this, &Visualize::symmetricDDALine);
+    connect(mPushButton5, &QPushButton::clicked, this, &Visualize::bazier);
+    connect(mPushButton6, &QPushButton::clicked, this, &Visualize::hermite);
     connect(mRenderer, SIGNAL(shapeUpdate()), mRenderer, SLOT(update()));
 }
 
@@ -22,14 +23,10 @@ Visualize::~Visualize()
 
 }
 
-//Slot for Implimentation of Symmetric DDA
 
 //Implimentation of UI
 void Visualize::setupUi() {
-
-    mCentralWidget = new QWidget(this);
-    mCentralWidget->setObjectName("centralwidget");
-
+    resize(1530, 785);
     mLabel1 = new QLabel(this);
     mLabel1->setObjectName("label");
     mLabel1->setText("Grid Size");
@@ -95,6 +92,92 @@ void Visualize::setupUi() {
     mPushButton4->setObjectName("pushButton");
     mPushButton4->setText("Draw Line (Symmetric DDA)");
     mPushButton4->setGeometry(QRect(870, 80, 200, 30));
+    
+    mPushButton5 = new QPushButton(this);
+    mPushButton5->setObjectName("pushButton");
+    mPushButton5->setText("Bazier Curve");
+    mPushButton5->setGeometry(QRect(1100, 80, 200, 30));
+
+    mPushButton6 = new QPushButton(this);
+    mPushButton6->setObjectName("pushButton");
+    mPushButton6->setText("Hermite Curve");
+    mPushButton6->setGeometry(QRect(1330, 80, 200, 30));
+
+    mLabel6 = new QLabel(this);
+    mLabel6->setObjectName("label");
+    mLabel6->setText("X1");
+    mLabel6->setGeometry(QRect(1130, 170, 85, 30));
+
+    mLineEdit6 = new QLineEdit(this);
+    mLineEdit6->setObjectName("lineEdit_7");
+    mLineEdit6->setGeometry(QRect(1100, 200, 85, 30));
+    
+    mLabel7 = new QLabel(this);
+    mLabel7->setObjectName("label");
+    mLabel7->setText("Y1");
+    mLabel7->setGeometry(QRect(1230, 170, 85, 30));
+
+    mLineEdit7 = new QLineEdit(this);
+    mLineEdit7->setObjectName("lineEdit_8");
+    mLineEdit7->setGeometry(QRect(1200, 200, 85, 30));
+
+    mLabel8 = new QLabel(this);
+    mLabel8->setObjectName("label");
+    mLabel8->setText("X2");
+    mLabel8->setGeometry(QRect(1130, 270, 85, 30));
+
+    mLineEdit8 = new QLineEdit(this);
+    mLineEdit8->setObjectName("lineEdit_9");
+    mLineEdit8->setGeometry(QRect(1100, 300, 85, 30));
+    
+    mLabel9 = new QLabel(this);
+    mLabel9->setObjectName("label");
+    mLabel9->setText("Y2");
+    mLabel9->setGeometry(QRect(1230, 270, 85, 30));
+
+    mLineEdit9 = new QLineEdit(this);
+    mLineEdit9->setObjectName("lineEdit_10");
+    mLineEdit9->setGeometry(QRect(1200, 300, 85, 30));
+
+    mLabel10 = new QLabel(this);
+    mLabel10->setObjectName("label");
+    mLabel10->setText("X3");
+    mLabel10->setGeometry(QRect(1130, 370, 85, 30));
+
+    mLineEdit10 = new QLineEdit(this);
+    mLineEdit10->setObjectName("lineEdit_11");
+    mLineEdit10->setGeometry(QRect(1100, 400, 85, 30));
+    
+    mLabel11 = new QLabel(this);
+    mLabel11->setObjectName("label");
+    mLabel11->setText("Y3");
+    mLabel11->setGeometry(QRect(1230, 370, 85, 30));
+
+    mLineEdit11 = new QLineEdit(this);
+    mLineEdit11->setObjectName("lineEdit_12");
+    mLineEdit11->setGeometry(QRect(1200, 400, 85, 30)); 
+
+    mLabel12 = new QLabel(this);
+    mLabel12->setObjectName("label");
+    mLabel12->setText("X4");
+    mLabel12->setGeometry(QRect(1130, 470, 85, 30));
+
+    mLineEdit12 = new QLineEdit(this);
+    mLineEdit12->setObjectName("lineEdit_13");
+    mLineEdit12->setGeometry(QRect(1100, 500, 85, 30));
+
+    mLabel13 = new QLabel(this);
+    mLabel13->setObjectName("label");
+    mLabel13->setText("Y4");
+    mLabel13->setGeometry(QRect(1230, 470, 85, 30));
+
+    mLineEdit13 = new QLineEdit(this);
+    mLineEdit13->setObjectName("lineEdit_13");
+    mLineEdit13->setGeometry(QRect(1200, 500, 85, 30));
+
+
+
+    
 
     mRenderer = new OpenGLWindow(QColor(0, 0, 0), this);
     mRenderer->setGeometry(QRect(90, 140, 1000, 600));
@@ -141,4 +224,42 @@ void Visualize::symmetricDDALine()
     Line l(Point2D(x1, y1), Point2D(x2, y2));
 
     mRenderer->symmetricDDA(l);
+}
+
+void Visualize::bazier() {
+    float x1 = mLineEdit6->text().toFloat();
+    float y1 = mLineEdit7->text().toFloat();
+    float x2 = mLineEdit8->text().toFloat();
+    float y2 = mLineEdit9->text().toFloat();
+    float x3 = mLineEdit10->text().toFloat();
+    float y3 = mLineEdit11->text().toFloat();
+    float x4 = mLineEdit12->text().toFloat();
+    float y4 = mLineEdit13->text().toFloat();
+
+    std::vector<Point2D> v;
+    v.push_back(Point2D(x1, y1));
+    v.push_back(Point2D(x2, y2));
+    v.push_back(Point2D(x3, y3));
+    v.push_back(Point2D(x4, y4));
+    
+    mRenderer->bazierCurve(v);
+}
+
+void Visualize::hermite() {
+    float x1 = mLineEdit6->text().toFloat();
+    float y1 = mLineEdit7->text().toFloat();
+    float x2 = mLineEdit8->text().toFloat();
+    float y2 = mLineEdit9->text().toFloat();
+    float x3 = mLineEdit10->text().toFloat();
+    float y3 = mLineEdit11->text().toFloat();
+    float x4 = mLineEdit12->text().toFloat();
+    float y4 = mLineEdit13->text().toFloat();
+
+    std::vector<Point2D> v;
+    v.push_back(Point2D(x1, y1));
+    v.push_back(Point2D(x2, y2));
+    v.push_back(Point2D(x3, y3));
+    v.push_back(Point2D(x4, y4));
+    
+    mRenderer->hermiteCurve(v);
 }
