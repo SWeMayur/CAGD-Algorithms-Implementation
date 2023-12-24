da6c7d1b1e86237f1350315b504f201663b6f25f
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2023-12-24 17:30:49 +0530

    Update OpenGLWindow.cpp for shader changes

--- Visualizer/OpenGLWindow.cpp
+++ Visualizer/OpenGLWindow.cpp
@@ -6,20 +6,23 @@
 #include <QPainter>
 #include "Grid.h"
 #include "Bresenhams.h"
+#include "Line.h"
 #include "SymmetricDDA.h"
 #include "BazierCurve.h"
 #include "HermiteCurve.h"
 
-OpenGLWindow::OpenGLWindow(const QColor& background, QMainWindow* parent):
-    mBackground(background)
+OpenGLWindow::OpenGLWindow(const QColor& background, QMainWindow* parent) : mBackground(background)
 {
     setParent(parent);
     setMinimumSize(720, 550);
+    const QStringList list = {"vertexShader.glsl" , "fragmentShader.glsl"};
+    mShaderWatcher = new QFileSystemWatcher(list, this);
+    connect(mShaderWatcher, &QFileSystemWatcher::fileChanged, this, &OpenGLWindow::shaderWatcher);
 }
 
-OpenGLWindow::~OpenGLWindow()
+OpenGLWindow::~OpenGLWindow()                                                                    
 {
-    reset();
+    reset(); 
 }
 
 void OpenGLWindow::reset()
@@ -40,8 +43,6 @@ void OpenGLWindow::reset()
 
 void OpenGLWindow::paintGL()
 {
-
-
     glClear(GL_COLOR_BUFFER_BIT);
 
     mProgram->bind();
@@ -49,24 +50,39 @@ void OpenGLWindow::paintGL()
     QMatrix4x4 matrix;
     matrix.ortho(-3.0f - (gridSize / 2), 3.0f + (gridSize / 2), -3.0f - (gridSize / 2), 3.0f + (gridSize / 2), 0.1f, 100.0f);
     matrix.translate(0, 0, -2);
-
+   // matrix.rotate(rotationAngle);
     mProgram->setUniformValue(m_matrixUniform, matrix);
+    std::vector<float> vert;
+    std::vector<float> col;
+    Grid grid(vert, col, gridSize);
 
-    Grid grid(vertices, colors, gridSize);
+    mProgram->setUniformValue("r", r);
+    mProgram->setUniformValue("g", g);
+    mProgram->setUniformValue("b", b);
+    mProgram->setUniformValue("angle", angle);
+
+
+    for (int i = 0; i < vert.size(); i++) {
+        vertices.append(vert[i]);
+    }
+    for (int i = 0; i < col.size(); i++) {
+        colors.append(col[i]);
+    }
+
     colorSqr(mPixelVertices);
 
-
-    glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, curveVertices.data());
-    glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, curveColors.data());
+    glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices.data());
+    glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors.data());
 
 
     glEnableVertexAttribArray(m_posAttr);
     glEnableVertexAttribArray(m_colAttr);
 
-    glDrawArrays(GL_LINES, 0, curveVertices.size() / 2);
+    glDrawArrays(GL_LINES, 0, vertices.size() / 2);
 
     glDisableVertexAttribArray(m_colAttr);
     glDisableVertexAttribArray(m_posAttr);
+
 }
 
 void OpenGLWindow::colorSqr(QVector<QVector2D>& pixels)
@@ -115,38 +131,84 @@ void OpenGLWindow::fillSquare(const QVector<QVector2D>& squareVertices, const QV
 
     glDisableVertexAttribArray(m_colAttr);
     glDisableVertexAttribArray(m_posAttr);
+    
+}
+
+QString OpenGLWindow::readShader(QString filePath){
+    QFile file(filePath);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return "Invalid file";
+
+    QTextStream stream(&file);
+    QString line = stream.readLine();
+
+    while (!stream.atEnd()) {
+        line.append(stream.readLine());
+    }
+    file.close();
+    return line;
+}
+
+void OpenGLWindow::shaderWatcher()
+{
+    QString vertexShaderSource = readShader("vertexShader.glsl");
+    QString fragmentShaderSource = readShader("fragmentShader.glsl");
+    
+    mProgram->release();
+    mProgram->removeAllShaders();
+    mProgram = new QOpenGLShaderProgram(this);
+    mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+    mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    mProgram->link();
 }
 
 void OpenGLWindow::initializeGL()
 {
-    static const char* vertexShaderSource =
-        "attribute highp vec4 posAttr;\n"
-        "attribute lowp vec4 colAttr;\n"
-        "varying lowp vec4 col;\n"
-        "uniform highp mat4 matrix;\n"
-        "void main() {\n"
-        "   col = colAttr;\n"
-        "   gl_Position = matrix * posAttr;\n"
-        "}\n";
-
-    static const char* fragmentShaderSource =
-        "varying lowp vec4 col;\n"
-        "void main() {\n"
-        "   gl_FragColor = col;\n"
-        "}\n";
-
+    QString vertexShaderSource = readShader("vertexShader.glsl");
+    QString fragmentShaderSource = readShader("fragmentShader.glsl");
+    
     initializeOpenGLFunctions();
 
     mProgram = new QOpenGLShaderProgram(this);
+
     mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
     mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
     mProgram->link();
     m_posAttr = mProgram->attributeLocation("posAttr");
     Q_ASSERT(m_posAttr != -1);
     m_colAttr = mProgram->attributeLocation("colAttr");
-    Q_ASSERT(m_colAttr != -1);
+  //  Q_ASSERT(m_colAttr != -1);
     m_matrixUniform = mProgram->uniformLocation("matrix");
     Q_ASSERT(m_matrixUniform != -1);
+}
+
+void OpenGLWindow::selectCurveColor()
+{
+    QColorDialog colorDialog(this);
+    QColor selectedColor = colorDialog.getColor();
+
+    if (selectedColor.isValid()){
+        // Set the selected color to the curves or store it for later use
+         r = selectedColor.redF();
+         g = selectedColor.greenF();
+         b = selectedColor.blueF();
+        emit shapeUpdate();  // Trigger an update to redraw the curves with the new color
+    }
+}
+
+void OpenGLWindow::mouseMoveEvent(QMouseEvent* event) {
+    int dx = event->x() - lastPos.x();
+    int dy = event->y() - lastPos.y();
+
+    if (event->buttons() & Qt::LeftButton) {
+        QQuaternion rotX = QQuaternion::fromAxisAndAngle(0.0f, 1.0f, 0.0f, 0.5f * dx);
+        QQuaternion rotY = QQuaternion::fromAxisAndAngle(1.0f, 0.0f, 0.0f, 0.5f * dy);
+
+        rotationAngle = rotX * rotY * rotationAngle;
+        update();
+    }
+    lastPos = event->pos();
 }
 
 void OpenGLWindow::addLine(Line l)
@@ -172,7 +234,15 @@ void OpenGLWindow::addGrid(int size)
 void OpenGLWindow::bresenhams(Line l)
 {
     mPixelVertices.clear();
-    Bresenhams b(l, mPixelVertices);
+    std::vector<std::vector<float>> mPV;
+    Bresenhams b(l, mPV);
+    for (std::vector<float> vect1D : mPV)
+    {
+        QVector2D q;
+        q.setX(vect1D[0]);
+        q.setY(vect1D[1]);
+        mPixelVertices.append(q);
+    }
     colorSqr(mPixelVertices);
     emit shapeUpdate();
 }
@@ -180,7 +250,15 @@ void OpenGLWindow::bresenhams(Line l)
 void OpenGLWindow::symmetricDDA(Line l)
 {
     mPixelVertices.clear();
-    SymmetricDDA s(l, mPixelVertices);
+    std::vector<std::vector<float>> mPV;
+    SymmetricDDA b(l, mPV);
+    for (std::vector<float> vect1D : mPV)
+    {
+        QVector2D q;
+        q.setX(vect1D[0]);
+        q.setY(vect1D[1]);
+        mPixelVertices.append(q);
+    }
     colorSqr(mPixelVertices);
     emit shapeUpdate();
 }
@@ -222,4 +300,9 @@ void OpenGLWindow::hermiteCurve(std::vector<Point2D> v)
         curveVertices.push_back(heC[i]);
     }
     emit shapeUpdate();
+}
+
+void OpenGLWindow::rotateObject(GLfloat rotationAngle)
+{
+    angle = rotationAngle;
 }
