535a7cfca1c318e21371058f6cba111a583ac152
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2023-12-25 21:49:28 +0530

    Deleted extra classes in visualizer and made libraries of them

--- Visualizer/Grid.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Grid
-{
-public:
-    Grid(QVector<GLfloat>& vertices, QVector<GLfloat>& colors, float gridSize);
-    ~Grid();
-private:
-    void storeGrid(QVector<GLfloat>& vertices, QVector<GLfloat>& colors, float gridSize);
-};
